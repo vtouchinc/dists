4c62bdbf74efd8b0be5f814a6e6d59a3c76ec129
ing03201 <ing03201.gmail.com>
2023-03-31 12:23:34 +0900

    add Release all directory

---
+++ update.sh
@@ -0,0 +1,23 @@
+DIST_NAME=("focal" "bionic")
+OS=("Ubuntu")
+CONTAINER=("main" "test")
+KEYNAME="dev@vtouch.io"
+for os in "${OS[@]}"; do
+    cd $os
+    for dist in "${DIST_NAME[@]}"; do
+        cd $dist
+        for container in "${CONTAINER[@]}"; do
+            cd $container
+            apt-ftparchive packages . > Packages
+            gzip -k -f Packages
+            apt-ftparchive release . > Release
+            cd ..
+        done
+        apt-ftparchive release . > Release
+        cd ..
+    done
+    cd ..
+done
+apt-ftparchive release . > Release
+gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
+gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
