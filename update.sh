a2cd3c6e971c668efd732450312f55a01aad84dd
ing03201 <ing03201.gmail.com>
2023-03-31 12:43:46 +0900

    change update.sh

--- update.sh
+++ update.sh
@@ -1,22 +1,27 @@
-DIST_NAME=("focal" "bionic")
-OS=("Ubuntu")
-CONTAINER=("main" "test")
+# DIST_NAME=("focal" "bionic")
+# OS=("Ubuntu")
+# CONTAINER=("main" "test")
 KEYNAME="dev@vtouch.io"
-for os in "${OS[@]}"; do
-    cd $os
-    for dist in "${DIST_NAME[@]}"; do
-        cd $dist
-        apt-ftparchive packages . > Packages
-        gzip -k -f Packages
-        apt-ftparchive release . > Release
-        gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
-        gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
-        cd ..
-    done
-    apt-ftparchive packages . > Packages
-    gzip -k -f Packages
-    apt-ftparchive release . > Release
-    gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
-    gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
-    cd ..
-done
+# for os in "${OS[@]}"; do
+#     cd $os
+#     for dist in "${DIST_NAME[@]}"; do
+#         cd $dist
+#         apt-ftparchive packages . > Packages
+#         gzip -k -f Packages
+#         apt-ftparchive release . > Release
+#         gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
+#         gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
+#         cd ..
+#     done
+#     apt-ftparchive packages . > Packages
+#     gzip -k -f Packages
+#     apt-ftparchive release . > Release
+#     gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
+#     gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
+#     cd ..
+# done
+apt-ftparchive packages . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
+gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
