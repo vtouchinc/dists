48a52e2f23cf53dc5b4abc6a1098ac662551497b
ing03201 <ing03201.gmail.com>
2023-03-31 12:31:35 +0900

    release file in distro name

--- update.sh
+++ update.sh
@@ -6,18 +6,12 @@ for os in "${OS[@]}"; do
     cd $os
     for dist in "${DIST_NAME[@]}"; do
         cd $dist
-        for container in "${CONTAINER[@]}"; do
-            cd $container
-            apt-ftparchive packages . > Packages
-            gzip -k -f Packages
-            apt-ftparchive release . > Release
-            cd ..
-        done
+        apt-ftparchive packages . > Packages
+        gzip -k -f Packages
         apt-ftparchive release . > Release
+        gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
+        gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
         cd ..
     done
     cd ..
 done
-apt-ftparchive release . > Release
-gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
-gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
